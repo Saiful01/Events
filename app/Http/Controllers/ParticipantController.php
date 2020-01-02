2f7de9fd43b32381e84a4cdc81ee3c436aadbb0f
saiful01 <saiful013101@gmail.com>
2020-01-02 16:20:55 +0600

    full

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -39,7 +39,7 @@ class ParticipantController extends Controller
      * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, $domPdf)
+    public function store(Request $request)
     {
 
         $qr_code = date('YmdHis');
